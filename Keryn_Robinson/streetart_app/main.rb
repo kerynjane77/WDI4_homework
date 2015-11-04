84eec92439297c28d9516a9746f85d8c21b35376
Keryn R <kerynjane77@gmail.com>
2015-11-04 17:22:50 +1100

    updated streetart app

--- Keryn_Robinson/streetart_app/main.rb
+++ Keryn_Robinson/streetart_app/main.rb
@@ -4,21 +4,35 @@ require 'sinatra/reloader'
 require 'pg' # to connect with postgres
 
 require_relative 'db_config'
+require_relative 'models/art_site'
 
-# do I need to add models/art_site
 
-# require_relative 'models/dish'
-
-# after do
-#   ActiveRecord::Base.connection.close
-# end
-
+# index (home page) and define variable art site
 get '/' do
+
+  @art_sites = ArtSite.all
   erb :index
+
 end
 
-# getting the form
+# getting the form to add art site
 get '/art_sites/new' do
+
   erb :new
 end
+
+# save new art site with params
+post '/art_sites'  do
+
+  @art_site = ArtSite.new
+  @art_site.artist_name = params[:artist_name]
+  @art_site.address = params[:address]
+  @art_site.art_photo = params[:art_photo]
+  @art_site.artist_url = params[:artist_url]
+  @art_site.save
+
+  redirect to '/'
+
+end
+
 
--- Keryn_Robinson/streetart_app/streetart_app.sql
+++ Keryn_Robinson/streetart_app/streetart_app.sql
@@ -5,9 +5,10 @@ CREATE TABLE art_sites
   id SERIAL4 PRIMARY KEY,
   artist_name VARCHAR(100) NOT NULL,
   address VARCHAR(400),
+  art_photo VARCHAR(100),
   artist_url VARCHAR(100) 
 
 );
 
-INSERT INTO art_sites (artist_name, address, artist_url) VALUES ('Baby Guerrilla', '70 Barkly Street', '');
+INSERT INTO art_sites (artist_name, address, art_photo, artist_url) VALUES ('Baby Guerrilla', '70 Barkly Street', '/img/baby_guerrila_VU.jpg', 'www.babyguerrilla.com');
 
